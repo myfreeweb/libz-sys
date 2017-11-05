881b610463fd7c1f1ba9f326f692c6572a58928a
Travis CI User <travis@example.org>
2017-11-05 19:14:40 +0000

    Documentation for alexcrichton/libz-sys@b521069

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libz_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"libz_sys/struct.gz_header.html\" title=\"struct libz_sys::gz_header\">gz_header</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"libz_sys/struct.z_stream.html\" title=\"struct libz_sys::z_stream\">z_stream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
